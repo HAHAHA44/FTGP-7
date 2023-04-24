1560726f5389a42e50eef5db4d375b5baacbfbe9
FrozenFireW <94864122+FrozenFireW@users.noreply.github.com>
2023-04-24 23:28:20 +0100

    Update Betting.sol

--- app/contracts/Betting.sol
+++ app/contracts/Betting.sol
@@ -14,12 +14,14 @@ contract Betting {
     // BetOption[] public betOptions;
     BetOrder[] public betOrders;
     //mapping(address => BetOrder[]) public betOrders;
-    mapping(address => uint256) public balances;
+    // mapping(address => uint256) public balances;
+    uint256 public balances;
     Bet[] public bets;
     //mapping(address => Bet[]) public bets;
     //当前的个选项倍率和订单ID
     uint[] public currentOdd; 
     uint[] public currentId;
+    
 
     //暂时没用
     //address[] public players;
@@ -39,6 +41,7 @@ contract Betting {
         uint256 odd;
         uint256 amount;
         uint256 prospectiveIncome;
+        uint256 Income;
         bool settled;
     }
 
@@ -58,17 +61,17 @@ contract Betting {
         uint option;
         address payable user;
         uint256 odds;
-        uint256 totalBetAmount;
+        uint256 orderAmount;
         uint256 betsPlacedAmount;
         uint256 prospectiveIncome;
         uint256 maxIncome;
+        uint256 Income;
         bool participation;
         bool settled;
         bool proceed;
-
     }
 
-    // 暂时有点用的构造器
+    // 构造器
     // constructor(uint _minimumBet, uint _maximumBet, uint _bettingTime, uint _numOfoptions)
     constructor(uint _numOfoptions) {
         owner = msg.sender;
@@ -82,10 +85,11 @@ contract Betting {
     }
     
     function createOrder(uint optionSelected, uint oddSetted) public payable {
-        require(msg.value > 10, "Insufficient deposit");
-
+        //由于gas费后面的创建要求将会提高
+        require(!ended , "Bet finsihed");
+        require(msg.value > 100 , "Insufficient deposit");
         require(msg.value % oddSetted == 0, "The amount and the odd are not divisible");
-        //require(oddSetted < 0 && oddSetted > 10, "Beyond the ratio range");
+        require(oddSetted > 0 && oddSetted < 100, "Beyond the ratio range");
         
         // 构建订单
         betOrders.push(BetOrder({
@@ -94,11 +98,12 @@ contract Betting {
             //ids:betOrders.length + 1,
             user: payable(msg.sender),
             option: optionSelected,
-            odds: oddSetted,
-            totalBetAmount: msg.value,
-            betsPlacedAmount: msg.value/oddSetted,
+            odds: oddSetted, //1=0.1倍 因为合约没有浮点数
+            orderAmount: msg.value,
+            betsPlacedAmount: (msg.value*10)/oddSetted,
             prospectiveIncome: 0,
-            maxIncome: (msg.value/oddSetted)*97/100, // 3% commission
+            maxIncome: (((msg.value*10)/oddSetted)+ msg.value)*97/100, // 3% commission
+            Income: 0,
             participation: false,
             settled: false,
             proceed: false
@@ -121,6 +126,7 @@ contract Betting {
     }
     
     function placeBet(uint256 optionSelected) public payable {
+        require(!ended , "Bet finsihed");
         require(betOrders.length > 0, "Array is empty");
         require(currentId[optionSelected] > 0, "Invalid bet option ID");
         BetOrder storage betOrder = betOrders[currentId[optionSelected]-1];
@@ -133,7 +139,7 @@ contract Betting {
             id: bets.length + 1,
             //投注者地址，结算的时候打钱到这里
             user: payable(msg.sender),
-            //庄家地址，结算的时候打钱到这里
+            //庄家地址
             banker:payable(betOrder.user),
             //投注者的订单ID
             betOrderId: betOrder.id,
@@ -144,12 +150,20 @@ contract Betting {
             //下注金额
             amount: msg.value,
             //预计收益
-            prospectiveIncome: msg.value * betOrder.odds*97/100,
+            prospectiveIncome: (msg.value/10 * betOrder.odds+ msg.value) *97/100,
+            Income: 0,
             //是否结算
             settled :false
         }));
         betOrder.participation = true;
-        betOrder.prospectiveIncome += msg.value*97/100;
+        if(betOrder.prospectiveIncome == 0){
+            betOrder.prospectiveIncome = betOrder.orderAmount*97/100;
+            betOrder.prospectiveIncome += msg.value*97/100;
+        }
+        else{
+            betOrder.prospectiveIncome += msg.value*97/100;
+        }
+        //寻找最大赔率订单索引 
         betOrder.betsPlacedAmount -= msg.value;
         if(betOrder.betsPlacedAmount == 0){
             uint maxOdd = 0;
@@ -174,18 +188,34 @@ contract Betting {
             if(betOrders[i].participation == true){
                 if(betOrders[i].option != optionSelected){
                     payable(betOrders[i].user).transfer(betOrders[i].prospectiveIncome);
+                    betOrders[i].Income = betOrders[i].prospectiveIncome;
                     betOrders[i].settled =true;
                 }
+                else{
+                    // 订单被部分投注
+                    if(betOrders[i].proceed == false){
+                        payable(betOrders[i].user).transfer((betOrders[i].betsPlacedAmount * betOrders[i].betsPlacedAmount)/10);
+                    }
+
+                }
+            }
+            else{
+                //全额退款可能考虑到gas费，目前100%退款
+                payable(betOrders[i].user).transfer(betOrders[i].orderAmount);
+                betOrders[i].settled =true;
             }
         }
         for(uint i=0 ;i< bets.length;i++){
-            if(bets[i].settled =false){
+            if(bets[i].settled == false){
                 if(bets[i].betOption == optionSelected){
                     payable(bets[i].user).transfer(bets[i].prospectiveIncome);
+                    balances = bets[i].user.balance;
+                    bets[i].Income = bets[i].prospectiveIncome;
                     bets[i].settled =true;
                     }
             }
         }
+        ended =true;
         
     }
 
