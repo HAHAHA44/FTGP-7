ffb9a2a720499fbcd61a2f1fc2c0f176fcd94557
Qinchen-Shi <119372818+Qinchen-Shi@users.noreply.github.com>
2023-05-08 00:08:20 +0100

    Update Main.sol

--- app/contracts/Main.sol
+++ app/contracts/Main.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Kdentifier: MIT
 
-import './GuessTheme.sol';
+import './Betting.sol';
 
 pragma solidity ^0.8.0;
 
@@ -38,64 +38,78 @@ contract Main {
     // function removeAdmin(address _Admin) public checkAdmin{
     // }
 
-    // login with metamask 备用
-    // function login() public view returns (address) {
-    //     return msg.sender;
-    // }
-
     // create a guess theme
-    function createGuessTheme(
+    function createGuessTHeme(
         string memory _name,
         string memory _description,
-        bool initial_pred,
         uint256 initial_pool,
-        uint256 _odd
-        ) public {
-            GuessTheme newTheme = new GuessTheme(           // link to GuessTheme.sol
-                msg.sender,
-                _name,
-                _description,
-                initial_pred,
-                initial_pool,
-                _odd
-            );
-            GuessThemes.push(address(newTheme));
+        uint256 _odd,
+        uint _options,
+        string memory _image,
+        string memory _source) public {
+        uint256 guess_id = GuessThemes.length + 1;          // create a guess id automatically and transform it into 8-digit string
+        bytes32 myBytes32 = bytes32(guess_id);
+        bytes memory myBytes = abi.encodePacked(myBytes32);
+        string memory guess_id_str = string(myBytes);
+        while (bytes(guess_id_str).length < 8) {
+            guess_id_str = string(abi.encodePacked('0', guess_id_str));
         }
+        
+        Betting newTheme = new Betting(          // create a new Betting contract
+            msg.sender,
+            guess_id_str,
+            _name,
+            _description,
+            initial_pool,
+            _odd,
+            _options,
+            _image,
+            _source
+        );
+        GuessThemes.push(address(newTheme));
+     }
     
-    // get all guess themes
-    function getAllGuessThemes() public view returns (address[] memory) {
-        return GuessThemes;
-    }
-
-    // get user's guess themes
-    function getUserGuessThemes() public view returns (address[] memory) {
-        address[] memory userGuessThemes = new address[](GuessThemes.length);
-        uint count = 0;
+    // get adresss of all guess themes 备用
+    // function getAllGuessThemesAds() public view returns (address[] memory) {
+    //     return GuessThemes;
+    // }
+    
+   // get all guess themes
+    function getAllGuessThemes() public view returns (string[] memory) {
+        string[] memory allGuessThemes = new string[](GuessThemes.length);      // store required details of guess themes
         for (uint i = 0; i < GuessThemes.length; i++) {
-            GuessTheme theme = GuessTheme(GuessThemes[i]);
-            if (theme.getOwner() == msg.sender) {
-                userGuessThemes[count] = GuessThemes[i];
-                count++;
-            }
+            Betting _theme = Betting(GuessThemes[i]);
+            allGuessThemes[i] = _theme.getBriefDetails();            // need something like getBriefDetails() in Betting
         }
-        address[] memory result = new address[](count);
-        for (uint i = 0; i < count; i++) {
-            result[i] = userGuessThemes[i];
-        }
-        return result;
+        return allGuessThemes;          //string[] 套 string[]，每个小的string[]是一个theme的信息
     }
 
-    // get user's guess themes
-    function getMyGuessThemes() public view returns (address[] memory) {
+    // get addresses of user's guess themes
+    function getMyGuessThemesAds() internal view returns (address[] memory) {       // 暂定internal
         address[] memory myGuessThemes = new address[](0);
         for (uint i = 0; i < GuessThemes.length; i++) {
-            GuessTheme _theme = GuessTheme(GuessThemes[i]);
-            if (_theme.getOwner() == msg.sender){               // need something like getOwner() in GuessTheme     
+            Betting _theme = Betting(GuessThemes[i]);
+            if (_theme.getOwner() == msg.sender){               // need something like getOwner() in Betting    
                 myGuessThemes.push(address(_theme));
             }
         }
         return myGuessThemes;
     }
+    
+    // get user's guess themes
+    function getMyGuessThemes() public view returns (string[] memory) {
+        address[] memory myGuessThemes = getMyGuessThemesAds();
+        string[] memory myGuessThemesBrief = new string[](myGuessThemes.length);
+        for (uint i = 0; i < myGuessThemes.length; i++) {
+            Betting _theme = Betting(myGuessThemes[i]);
+            myGuessThemesBrief[i] = _theme.getBriefDetails();
+        }
+        return myGuessThemesBrief;          // string[]
+    }
 
-  
+  // get all details of a guess theme
+    function getGuessThemeDetails(address _guessTheme) public view returns (string memory) {   // 需要传入一个address
+        Betting _theme = Betting(_guessTheme);
+        return _theme.getDetails();         // string[]
+    }
 }
